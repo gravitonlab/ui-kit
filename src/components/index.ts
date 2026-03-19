affa433667b58d51950c31bc054bc666b776706c
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2026-03-19 15:05:34 +0500

    refactor: simplify component exports using wildcard syntax
    Replace individual named imports and exports with wildcard exports for cleaner and more maintainable code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-import { Button } from "./Button";
-import { Header } from "./Header";
-import { Page } from "./Page";
-
-export { Button, Header, Page };
+export * from "./Button";
+export * from "./Header";
+export * from "./Page";
